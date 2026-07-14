d4ff3abb09a20166e2af6c1746ad4fa55b19b9bd
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-14 09:58:40 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+/* ============================================
+ * CONFIG — deploy ke time sirf yeh file badalni hai
+ * ============================================ */
+
+/* GAS Web App URL yahan paste karo */
+var API_URL = 'https://script.google.com/macros/s/AKfycbw3_j9xdT7aR8Ovx8CEqPzZidJnn6vKOjxHQEzvu3DhVzvaZzU2Mo7QCKMpiUlCJVydkg/exec';
