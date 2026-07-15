66d1ec5f7e07b77607bde1bed552882f16d7b4fc
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-15 10:34:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * ============================================ */
 
 /* GAS Web App URL yahan paste karo */
-var API_URL = 'https://script.google.com/macros/s/AKfycbzTySZ1Sxpnv1fkNDIfP88_ASOnvVdG9EubBWCSPoNKBbykoDNGFbRsy3wwy0aPiq8IQQ/exec';
+var API_URL = 'https://script.google.com/macros/s/AKfycbw1ZZOZEA-46nBrGcIHvbSrBoD1n6ujx53yGhxyaeVAo7WCvowwJ3HuZJYcNq9YrhcWGA/exec';
